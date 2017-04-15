4867c3fa54192fc20aeb1a655e338df095520e3a
shenn13 <shenn13@163.com>
2017-04-15 18:04:38 +0800

    no message

---
+++ SDAutoLayout-master/SDAutoLayout-master/Main/MainModel.h
@@ -0,0 +1,13 @@
+//
+//  MainModel.h
+//  SDAutoLayout-master
+//
+//  Created by shen on 17/4/15.
+//  Copyright © 2017年 shen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MainModel : NSObject
+
+@end
---
+++ SDAutoLayout-master/SDAutoLayout-master/Main/MainModel.m
@@ -0,0 +1,13 @@
+//
+//  MainModel.m
+//  SDAutoLayout-master
+//
+//  Created by shen on 17/4/15.
+//  Copyright © 2017年 shen. All rights reserved.
+//
+
+#import "MainModel.h"
+
+@implementation MainModel
+
+@end
