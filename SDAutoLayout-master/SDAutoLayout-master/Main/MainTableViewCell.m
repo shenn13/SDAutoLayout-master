f6fd640dcb9d23ddfbb44a83c6e2c4282ed9250b
shenn13 <shenn13@163.com>
2017-04-15 22:28:03 +0800

    no message

--- SDAutoLayout-master/SDAutoLayout-master/Main/MainTableViewCell.m
+++ SDAutoLayout-master/SDAutoLayout-master/Main/MainTableViewCell.m
@@ -18,6 +18,8 @@
 #define kScreenWidth [UIScreen mainScreen].bounds.size.width
 #define kScreenHeight [UIScreen mainScreen].bounds.size.height
 
+#define W(x) kScreenWidth*(x)/320.0
+#define H(y) kScreenHeight*(y)/568.0
 #define kMarg 10
 #define kHeightLabel 30
 
@@ -67,10 +69,7 @@
 
     _contentLabel.sd_layout.topSpaceToView(_titleLabel, kMarg).rightSpaceToView(self.contentView, kMarg * 2).leftSpaceToView(self.contentView,kMarg * 2).autoHeightRatio(0);
     
-    _imageView.sd_layout.topSpaceToView(_contentLabel,kMarg).rightSpaceToView(self.contentView, kMarg * 12).leftSpaceToView(self.contentView,kMarg * 12).heightIs(230);
-    
-
-  
+    _imageView.sd_layout.topSpaceToView(_contentLabel,kMarg).rightSpaceToView(self.contentView, kMarg *10).widthIs(kScreenWidth - kMarg*20).heightIs(H(200));
 }
 
 
@@ -82,8 +81,6 @@
     _imageView.image = [UIImage imageNamed:model.image];
     
     //***********************高度自适应cell设置步骤************************
-    
-    
     [self setupAutoHeightWithBottomView:_imageView bottomMargin:20];
     
 }
